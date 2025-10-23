8d9154b1a607fbb8044afab0d59a7418089e0883
K-Siim <kevinsiim007@gmail.com>
2025-10-23 13:55:13 +0300

    Added Pagination in the Index.vue

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class Post extends Model
 {
+
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'content',
